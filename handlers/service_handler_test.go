b5c417a5e3e5a86d03065cd79d36a3ac3d2d674c
iggymacd <amacdonald@thecape.ca>
2015-05-09 07:14:13 -0300

    added template code

--- handlers/service_handler_test.go
+++ handlers/service_handler_test.go
@@ -8,6 +8,35 @@ import (
 	"testing"
 )
 
+func TestTextHandler(t *testing.T) {
+	handler := new(TextHandler)
+	expectedBody := `
+John Smith is 22 years old.
+
+Alice Smith is 25 years old.
+
+Bob Baker is 24 years old.
+`
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost/hello?say=%s", expectedBody), nil)
+	if err != nil {
+		t.Errorf("Failed to create request.")
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	switch recorder.Body.String() {
+	case expectedBody:
+		// body is equal so no need to do anything
+	default:
+		t.Errorf("Body (%s) did not match expectation (%s).",
+			recorder.Body.String(),
+			expectedBody)
+	}
+}
+
 func TestEchosContent(t *testing.T) {
 	handler := new(HelloHandler)
 	expectedBody := "hellooo!"
@@ -31,8 +60,8 @@ func TestEchosContent(t *testing.T) {
 	}
 }
 
-/*func TestReturns404IfYouSayNothing(t *testing.T) {
-	handler := new(EchoHandler)
+func TestReturns404IfYouSayNothing(t *testing.T) {
+	handler := new(HelloHandler)
 
 	recorder := httptest.NewRecorder()
 
@@ -49,7 +78,7 @@ func TestEchosContent(t *testing.T) {
 }
 
 func TestClient(t *testing.T) {
-	server := httptest.NewServer(new(EchoHandler))
+	server := httptest.NewServer(new(HelloHandler))
 	defer server.Close()
 
 	// Pretend this is some sort of Go client...
@@ -62,4 +91,4 @@ func TestClient(t *testing.T) {
 	if resp.StatusCode != 404 {
 		t.Errorf("Did not get a 404.")
 	}
-}*/
+}
