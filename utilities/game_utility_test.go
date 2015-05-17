cb318e51b55f472fd3562eb923ffc98776019f9b
Allan MacDonald <iggymacd@gmail.com>
2015-05-17 11:22:37 -0300

    combinators

--- utilities/game_utility_test.go
+++ utilities/game_utility_test.go
@@ -55,9 +55,71 @@ func TestPermutations(t *testing.T) {
 	west = remove(0, west)
 	t.Logf("West Cards are %v, capacity is %d", west, cap(west))
 
+	allCardsInRound := [][]deck.Card{north, east, south, west}
+	t.Logf("all cards :: %v", allCardsInRound)
+	/*for _, h := range allCardsInRound {
+		for i, cell := range h {
+			t.Logf("card is %v at position %d", cell, i)
+		}
+		t.Log("looping ...")
+	}*/
+
+	for i, h := range allCardsInRound {
+		/*for i, cell := range h {
+			t.Logf("card is %v at position %d", cell, i)
+		}*/
+		t.Logf("Length before removal of first is %d, capacity is %d", len(h), cap(h))
+		allCardsInRound[i] = remove(0, allCardsInRound[i])
+		//t.Logf("card is %v at position 0", h[0])
+		t.Logf("Length after removal of first is %d, capacity is %d", len(h), cap(h))
+		t.Log("looping ...")
+	}
+	t.Logf("all cards :: %v", allCardsInRound)
 	//}
 }
 
+func playRound(nextCardToPlay int, allCards [][]deck.Card, currentResult []deck.Card) {
+
+}
+
+//////////////////////////////
+//from http://stackoverflow.com/questions/19249588/go-programming-generating-combinations
+func GenerateCombinations(alphabet string, length int) <-chan string {
+	c := make(chan string)
+
+	// Starting a separate goroutine that will create all the combinations,
+	// feeding them to the channel c
+	go func(c chan string) {
+		defer close(c) // Once the iteration function is finished, we close the channel
+
+		AddLetter(c, "", alphabet, length) // We start by feeding it an empty string
+	}(c)
+
+	return c // Return the channel to the calling function
+}
+
+// AddLetter adds a letter to the combination to create a new combination.
+// This new combination is passed on to the channel before we call AddLetter once again
+// to add yet another letter to the new combination in case length allows it
+func AddLetter(c chan string, combo string, alphabet string, length int) {
+	// Check if we reached the length limit
+	// If so, we just return without adding anything
+	if length <= 0 {
+		return
+	}
+
+	var newCombo string
+	for _, ch := range alphabet {
+		newCombo = combo + string(ch)
+		if len(newCombo) == 4 {
+			c <- newCombo
+		}
+		AddLetter(c, newCombo, alphabet, length-1)
+	}
+}
+
+//from http://stackoverflow.com/questions/19249588/go-programming-generating-combinations
+////////////////////////////
 func remove(element int, source []deck.Card) []deck.Card {
 	source = append(source[:element], source[element+1:]...)
 	return source
